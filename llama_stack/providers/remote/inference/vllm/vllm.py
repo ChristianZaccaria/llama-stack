54fb0665da9e21ac57ee53c39e87fb96be83d481
ChristianZaccaria <christian.zaccaria.cz@gmail.com>
2025-05-02 12:26:02 +0100

    f

--- llama_stack/providers/remote/inference/vllm/vllm.py
+++ llama_stack/providers/remote/inference/vllm/vllm.py
@@ -14,7 +14,7 @@ from openai.types.chat.chat_completion_chunk import (
     ChatCompletionChunk as OpenAIChatCompletionChunk,
 )
 
-# test #2 pass
+# test #3 pass
 from llama_stack.apis.common.content_types import (
     InterleavedContent,
     InterleavedContentItem,
