8b1ba306a83a8365f0548cb450a54442f744b4c7
ChristianZaccaria <christian.zaccaria.cz@gmail.com>
2025-05-02 11:18:09 +0100

    ff

--- llama_stack/providers/remote/inference/vllm/vllm.py
+++ llama_stack/providers/remote/inference/vllm/vllm.py
@@ -2,7 +2,7 @@
 # All rights reserved.
 #
 # This source code is licensed under the terms described in the LICENSE file in
-# the root directory of this source tree. test #2
+# the root directory of this source tree. test #3
 import json
 import logging
 from collections.abc import AsyncGenerator, AsyncIterator
